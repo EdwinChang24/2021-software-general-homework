135ef4329cc33f2d6d1adcebf5d590ea5a71d7c3
NoPro2024 <edwinchang2024+githubpublic@gmail.com>
2021-08-28 10:13:51 -0700

    Revert "Complete lesson 1"
    This reverts commit ad633665ccfe1e2370b1bd881425b2fbe7ca8acd.

--- lesson_1.py
+++ lesson_1.py
@@ -1,24 +1,8 @@
 # Create a program in which the user creates 
 # a specific FRC team and store the following variables:
-#   Team Number (named team_number)
-#   Team Name (named team_name)
-#   Location (named location)
-#   Rookie Year (named rookie_year)
-#   Is Active (named is_active)
+	# Team Number (named team_number)
+	# Team Name (named team_name)
+	# Location (named location)
+	# Rookie Year (named rookie_year)
+	# Is Active (named is_active)
 # Be sure to store each variable as the correct type!
-
-def main():
-    # Team Number
-    team_number: int = 1678
-    # Team Name
-    team_name: str = "Citrus Circuits"
-    # Location
-    location: str = "Davis, CA"
-    # Rookie Year
-    rookie_year: int = 2004
-    # Is Active
-    is_active: bool = True
-
-
-if __name__ == "__main__":
-    main()
